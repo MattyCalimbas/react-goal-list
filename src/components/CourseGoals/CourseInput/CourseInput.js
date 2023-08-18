c7d42f0ae58f77c4629322af0dbbf7f90e401573
Matt Calimbas <matthewcalimbas@gmail.com>
2023-08-18 10:52:35 -0700

    refactored CourseInput to use CSS Modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,38 +1,38 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from'./CourseInput.module.css';
 
 
 // & input is recieving invalid prop from component call within return and running arrow function to dynmically change css value within styled component
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${props => props.invalid ? 'red':'black'}
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color: ${props => props.invalid ? 'red':'black'}
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => props.invalid ? 'red':'#ccc'};
-  background: ${props => props.invalid ? '#ffd7d7':'transparent'};
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props => props.invalid ? 'red':'#ccc'};
+//   background: ${props => props.invalid ? '#ffd7d7':'transparent'};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
-`;
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
+// `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -56,13 +56,15 @@ const CourseInput = props => {
   };
 
   // using template literal to dynamically change string variable to contain invalid if data input pulls error
-  // FormControl is using invalid prop to track true/false value of isValid.  If invalid changes to true (isValid), styled component above has callback functions using invalid prop
+  // invalid prop to track true/false value of isValid.  If invalid changes to true (isValid), styled component above has callback functions using invalid prop
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      {/* to access property use square brackets to access property names with dashes */}
+      {/* using && ternary operation where if first argument is true return what is after &&, this is a way to implement dynamic classes with CSS Modules*/}
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`} invalid={!isValid}>
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
